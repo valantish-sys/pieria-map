254193eefe83253979ffba655cfd2309b83cbdbf
valantish-sys <valantish@yahoo.gr>
2026-07-08 18:33:52 +0300

    Update slide.js

--- slide.js
+++ slide.js
@@ -1,4 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
+    // Μικρή καθυστέρηση 500ms για να είναι σίγουρο ότι όλα τα div έχουν σχεδιαστεί
+    setTimeout(() => {
+        console.log("Ξεκινάω το slider...");
+        // ΕΔΩ ΒΑΖΕΙΣ ΟΛΟ ΤΟΝ ΥΠΟΛΟΙΠΟ ΚΩΔΙΚΑ ΣΟΥ
+    }, 500);
+});
   // --- ΠΡΟΣΘΗΚΗ ΕΔΩ: Ανίχνευση Κινητού για όλο το Script ---
   let isTouchDevice = false;
   let isPausedByTitle = false; // ΝΕΟ: Ελέγχει αν το slider έχει παγώσει
