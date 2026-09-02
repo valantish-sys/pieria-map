a0baadffa45f6f572942adac4c7f97c6de565303
valantish-sys <valantish@yahoo.gr>
2026-09-02 07:25:11 +0300

    Update polla20.js

--- polla20.js
+++ polla20.js
@@ -10618,7 +10618,7 @@ if (document.readyState === "loading") {
      
                 if (diffDays <= 365) {
             
-                    if (Math.random() > 0.20 && retryCount < 10) {
+                    if (Math.random() > 0.15 && retryCount < 10) {
                         return await MobileDataEngine.fetchPosts(retryCount + 1); // Αθόρυβη νέα κλήρωση!
                     }
                 }
